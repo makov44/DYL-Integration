615078b18dadcf2cc5b4632f4645aa005926c8ea
sergiy <smakovskyy@dyl.com>
2017-05-10 12:59:20 -0700

    1. Added navigation buttons

---
+++ WpfApplication1/Helpers/ImageLoader.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace DYL.EmailIntegration.UI.Helpers
+{
+    public static class ImageLoader
+    {
+        public static ImageSource GetDefaultImage(DependencyObject obj)
+        {
+            return (ImageSource)obj.GetValue(DefaultImageProperty);
+        }
+        public static void SetDefaultImage(DependencyObject obj, ImageSource value)
+        {
+            obj.SetValue(DefaultImageProperty, value);
+        }
+
+        public static readonly DependencyProperty DefaultImageProperty =
+            DependencyProperty.RegisterAttached(
+            "DefaultImage",
+            typeof(ImageSource),
+            typeof(ImageLoader),
+            new UIPropertyMetadata(null));
+
+        public static ImageSource GetHoverImage(DependencyObject obj)
+        {
+            return (ImageSource)obj.GetValue(HoverImageProperty);
+        }
+        public static void SetHoverImage(DependencyObject obj, ImageSource value)
+        {
+            obj.SetValue(HoverImageProperty, value);
+        }
+
+        public static readonly DependencyProperty HoverImageProperty =
+            DependencyProperty.RegisterAttached(
+            "HoverImage",
+            typeof(ImageSource),
+            typeof(ImageLoader),
+            new UIPropertyMetadata(null));
+
+        public static ImageSource GetDisabledImage(DependencyObject obj)
+        {
+            return (ImageSource)obj.GetValue(DisabledImageProperty);
+        }
+        public static void SetDisabledImage(DependencyObject obj, ImageSource value)
+        {
+            obj.SetValue(DisabledImageProperty, value);
+        }
+
+        public static readonly DependencyProperty DisabledImageProperty =
+            DependencyProperty.RegisterAttached(
+            "DisabledImage",
+            typeof(ImageSource),
+            typeof(ImageLoader),
+            new UIPropertyMetadata(null));
+    }
+}
